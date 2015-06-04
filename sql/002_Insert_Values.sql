215b5948b222e5a214e2156f83c1f96f4e783d4b
Joaquin Tarazona <joaquin.tarazona@ec.pe>
2015-06-04 17:34:57 -0500

    Developer corrections All

--- sql/002_Insert_Values.sql
+++ sql/002_Insert_Values.sql
@@ -1,11 +1,9 @@
 -- users data with password = md5('admin')
-
 INSERT INTO `auth_rol` (`rol_desc`) VALUES ('Adminstrador');
 
-INSERT INTO `auth_personal` (empcod,percod,codigo) VALUES ('001','0000154','DHH');
-
-INSERT INTO `auth_usuario` (`us_usuario`, `us_password`,us_email,fecha_creacion,us_nombre,us_apellidos,rol_id,pers_id) VALUES
-('admin', md5('tarazona'),'jmike410@gmail.com',now(),'Joaquin','Tarazona Aguirre',1,1);
+INSERT INTO `auth_usuario` (`us_usuario`, `us_password`,us_email,fecha_creacion,us_nombre,us_apellidos,rol_id,pers_id) 
+select 'admin', md5('tarazona'),'jmike410@gmail.com',now(),'Joaquin','Tarazona Aguirre',1, 
+(select pers_id from auth_personal where percod = '0000154' and codigo = 'DHH');
 
 insert into sys_recurso (recurso_desc,recurso_uri,recurso_estado,recurso_tipo,rec_recurso_id) values 
 ('Usuario','usuario',1,1,NULL);
@@ -17,10 +15,10 @@ insert into sys_recurso (recurso_desc,recurso_uri,recurso_estado,recurso_tipo,re
 ('Reportes','reporte',1,1,NULL);
 
 insert into sys_rol_recurso (recurso_id,rol_id,rolrec_permiso)
-select (select recurso_id from sys_recurso where recurso_desc = 'Usuario' and recurso_uri is null and recurso_tipo = 1),1,'allow';
+select (select recurso_id from sys_recurso where recurso_desc = 'Usuario' and recurso_tipo = 1),1,'allow';
 
 insert into sys_rol_recurso (recurso_id,rol_id,rolrec_permiso)
-select (select recurso_id from sys_recurso where recurso_desc = 'Reportes' and recurso_uri is null and recurso_tipo = 1),1,'allow';
+select (select recurso_id from sys_recurso where recurso_desc = 'Reportes' and recurso_tipo = 1),1,'allow';
 
 insert into sys_rol_recurso (recurso_id,rol_id,rolrec_permiso)
 select (select recurso_id from sys_recurso where recurso_uri = 'adm:usuario:lista' and recurso_tipo = 1),1,'allow';
