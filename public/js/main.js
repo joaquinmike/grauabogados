1d79771be84fab063e9ba757545c269c15cc2918
Joaquin Tarazona <joaquin.tarazona@ec.pe>
2015-06-19 09:57:48 -0500

    Developer corrections filter personal

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,4 @@
  */
 function getRestoreFilter(url){
     location.href = url
-    }
+}
--- web/Application/src/Application/Controller/UsuarioController.php
+++ web/Application/src/Application/Controller/UsuarioController.php
@@ -9,31 +9,47 @@
 
 namespace Application\Controller;
 
+use Zend\Session\Container;
 use Util\Controller\BaseController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class UsuarioController  extends BaseController {
     
     public function listaAction(){
         $page = $this->params()->fromQuery('page', 1); 
+        $filtro = $this->params()->fromQuery('filtro', NULL); 
         $post = array();
         $formFilter = $this->getServiceLocator()->get('Form\FormUsuarioFiltro');
-        //if($this->getRequest()->isPost()){
-            $post = $this->params()->fromQuery();
-            $formFilter->setData($post);
-        //}
-        
+        $sesPersonal = new Container('personal');    
+        if($this->getRequest()->isPost()){
+            $post = $this->params()->fromPost();
+            $sesPersonal->filter =  $post;
+        }
+        if(empty($sesPersonal->filter)){
+            $sesPersonal->filter = array();
+        }else{
+            $formFilter->setData($sesPersonal->filter);
+        }
         $item = 1;
         if($page > 1){
             $item = (($page - 1) * \Application\Entity\Functions::LIMIT_DEFAULT) + 1;
         }
         $modelPersonal = $this->getServiceLocator()->get('Model\AuthPersonal');
-        $paginator = $modelPersonal->getPersonalAllByOrder($page,$post);
+        $paginator = $modelPersonal->getPersonalAllByOrder($page,$sesPersonal->filter);
         
         return new ViewModel(array(
             'item' => $item,
             'formFilter' => $formFilter,
             'paginator' => $paginator
         ));
+    }
+    
+    public function listaLimpiarAction(){
+        $sesPersonal = new Container('personal');    
+        $sesPersonal->filter = array();
+        $json = new JsonModel( array('success' => 1) );
+        return $json;
+        
     }
 }
