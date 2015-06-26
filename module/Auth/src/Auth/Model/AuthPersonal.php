fd12d915b5a6fd5b6b816755ac72c7232fd2190e
unknown <jmike410@gmail.com>
2015-06-26 17:38:22 -0500

    Developer add graficos

--- module/Auth/src/Auth/Model/AuthPersonal.php
+++ module/Auth/src/Auth/Model/AuthPersonal.php
@@ -84,7 +84,7 @@ class AuthPersonal extends AbstractRepository {
      public function getGraficoPersonalByCategoria($areaCode, $fechaIni, $fechaFin){
         $select = $this->sql->select()->from(array('t1' => 'v_grafico_horas_x_cliente'),
                array('cliente','tiempo' => 'sum(tiemponum) '))
-           ->where(array('areacod  => ?' => $areaCode))
+           ->where(array('areacod  = ?' => $areaCode))
            ->where(array('anomes BETWEEN ? and ?' => array($fechaIni,$fechaFin)))
            ->group(array('cliente'))
             ->order('cliente');
